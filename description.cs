6fe3498cb65628ef7fcd93986e7d0980675b3809
q1225 <46804537+q1225@users.noreply.github.com>
2023-04-01 19:47:45 -0400

    Add files via upload

---
+++ description.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class description : MonoBehaviour
+{ //light or dark, move in one direction until wall, health indicator?, base damage *damage multiplyer
+  
+    
+    // Start is called before the first frame update
+    void Start()
+    { 
+        bool light; 
+        double bh,bd,multiplyer,td=bd*multiplyer
+        while(0<bh<100):
+         if light==0;
+         bh=bh+td
+         else 
+          bh=bh-td    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
