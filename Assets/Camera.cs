014e72482f51d5edf3cece8677cb8a9be998e4e0
Gabriel Ford <gabe@fordltc.net>
2023-04-01 04:37:14 -0400

    integrate movement in patrick's map

--- Assets/Camera.cs
+++ Assets/Camera.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
+    private const float height = (float)(0.5);
     public int[,] positionMatrix;
     float time_elapsed = 0;
     // Start is called before the first frame update
     void Start()
     {
-        positionMatrix = new int[4,4];
-        positionMatrix[0,0] = 1;
+        positionMatrix = new int[50,50];
+        positionMatrix[1,0] = 1;
     }
 
     // Update is called once per frame
@@ -19,22 +20,8 @@ public class Camera : MonoBehaviour
         time_elapsed += Time.deltaTime;
         if (time_elapsed >= 1)
         {
-            print("hit");
             time_elapsed = 0;
-            for (int i = 0; i < positionMatrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < positionMatrix.GetLength(1); j++)
-                {
-                    print(i);
-                    print(j);
-                    if (positionMatrix[i, j] == 1)
-                    {
-                        positionMatrix[i,j+1] = 1;
-                        positionMatrix[i, j] = 0;
-                        break;
-                    }
-                }
-            }
+            moveForward();
         }
         for (int i = 0; i < positionMatrix.GetLength(0); i++)
         {
@@ -43,12 +30,28 @@ public class Camera : MonoBehaviour
                 if (positionMatrix[i, j] == 1)
                 {
                     Vector3 newPos = new Vector3();
-                    newPos.x = 5 + 10 * i;
-                    newPos.z = 5 + 10 * j;
-                    newPos.y = 4;
+                    newPos.x = (float)(.5 + 1 * i);
+                    newPos.z = (float)(.5 + 1 * j);
+                    newPos.y = height;
                     transform.position = newPos;
                 }
              }
+        }
+    }
+
+    void moveForward()
+    {
+        for (int i = 0; i < positionMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < positionMatrix.GetLength(1); j++)
+            {
+                if (positionMatrix[i, j] == 1)
+                {
+                    positionMatrix[i, j + 1] = 1;
+                    positionMatrix[i, j] = 0;
+                    break;
+                }
+            }
         }
     }
 }
