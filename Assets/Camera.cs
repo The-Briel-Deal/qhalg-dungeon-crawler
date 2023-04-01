50a15cb76ebc167f7c1a85abec12397e89b5a653
Gabriel Ford <gabe@fordltc.net>
2023-04-01 03:40:48 -0400

    Add initial floor grid and camera script

---
+++ Assets/Camera.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[,] positionMatrix = new int[4,4];
+        // for(int i = 0; i < positionMatrix.GetLength(0); i++)
+        // {
+        //     for(int j = 0; j < positionMatrix.GetLength(1); j++)
+        //     {
+        //     }
+        // }
+        positionMatrix[0,0] = 1
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
