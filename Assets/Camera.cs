1baf60557c07146b1e10dd46f38e26ffe7324584
Gabriel Ford <gabe@fordltc.net>
2023-04-04 22:16:28 -0400

    Redid positionMatrix

--- Assets/Camera.cs
+++ Assets/Camera.cs
@@ -17,7 +17,7 @@ public class Camera : MonoBehaviour
     private bool moving = false;
 
     private const float height = (float)(0.5);
-    private int[,] positionMatrix;
+    private GridObject[,] positionMatrix;
     // float time_elapsed = 0;
     // Start is called before the first frame update
     void Start()
@@ -26,7 +26,7 @@ public class Camera : MonoBehaviour
         {
             for (int j = 0; j < positionMatrix.GetLength(1); j++)
             {
-                if (positionMatrix[i, j] == 1)
+                if (positionMatrix[i, j].Type == 1)
                 {
                     Vector3 newPos = new Vector3();
                     newPos.x = (float)(.5 + 1 * i);
@@ -90,9 +90,10 @@ public class Camera : MonoBehaviour
         {
             for (int j = 0; j < positionMatrix.GetLength(1); j++)
             {
-                if (positionMatrix[i, j] == 1)
+                //print(positionMatrix[0,0]);
+                if (positionMatrix[i, j].Type == 1)
                 {
-                    if (positionMatrix[i, j + 1] == 3 && !inCombat)
+                    if (positionMatrix[i, j + 1].Type == 3 && !inCombat)
                     {
                         print("going into combat!");
                         initiateCombat();
@@ -125,30 +126,30 @@ public class Camera : MonoBehaviour
         {
             for (int j = 0; j < positionMatrix.GetLength(1); j++)
             {
-                if (positionMatrix[i, j] == 1)
+                if (positionMatrix[i, j].Type == 1)
                 {
                     float direction = (((int)(transform.rotation.eulerAngles.y)+directionAdjustment) / 90) % 4;
                     print(direction);
                     if (direction == 0)
                     {
                         // if position we are moving to is a wall, do not move there.
-                        if (positionMatrix[i, j + 1] == 2)
+                        if (positionMatrix[i, j + 1].Type == 2)
                         {
                             print("0");
                             return;
                         }
-                        positionMatrix[i, j + 1] = 1;
-                        positionMatrix[i, j] = 0;
+                        positionMatrix[i, j + 1].Type = 1;
+                        positionMatrix[i, j].Type = 0;
                     } else if (direction == 1)
                     {
                         // if position we are moving to is a wall, do not move there.
-                        if (positionMatrix[i+1, j] == 2)
+                        if (positionMatrix[i+1, j].Type == 2)
                         {
                             print("1");
                             return;
                         }
-                        positionMatrix[i + 1, j] = 1;
-                        positionMatrix[i, j] = 0;
+                        positionMatrix[i + 1, j].Type = 1;
+                        positionMatrix[i, j].Type = 0;
                     } else if (direction == 2)
                     {
                         if (j - 1 < 0)
@@ -157,13 +158,13 @@ public class Camera : MonoBehaviour
                             return;
                         }
                         // if position we are moving to is a wall, do not move there.
-                        if (positionMatrix[i, j - 1] == 2)
+                        if (positionMatrix[i, j - 1].Type == 2)
                         {
                             print("3");
                             return;
                         }
-                        positionMatrix[i, j - 1] = 1;
-                        positionMatrix[i, j] = 0;
+                        positionMatrix[i, j - 1].Type = 1;
+                        positionMatrix[i, j].Type = 0;
                     }
                     else if (direction == 3)
                     {
@@ -172,12 +173,12 @@ public class Camera : MonoBehaviour
                             return;
                         }
                         // if position we are moving to is a wall, do not move there.
-                        if (positionMatrix[i - 1, j] == 2)
+                        if (positionMatrix[i - 1, j].Type == 2)
                         {
                             return;
                         }
-                        positionMatrix[i - 1, j] = 1;
-                        positionMatrix[i, j] = 0;
+                        positionMatrix[i - 1, j].Type = 1;
+                        positionMatrix[i, j].Type = 0;
                     }
 
                     return;
--- Assets/Map.cs
+++ Assets/Map.cs
@@ -2,20 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class GridObject
+{
+    // Declare properties
+    public int Type { get; set; }
+    public GameObject Object { get; set; }
+
+    // Declare constructor(s)
+    public GridObject(int type = 0, GameObject gObject = null)
+    {
+        Object = gObject;
+        Type = type;
+    }
+
+    // Declare methods
+    public void PrintInfo()
+    {
+        Debug.Log($"Object: {Object}, Type: {Type}");
+    }
+}
+
 public class Map : MonoBehaviour
 {
-    public int[,] positionMatrix;
+    public GridObject[,] positionMatrix;
     public GameObject skeletonPrefab;
     void Start()
     {
-        positionMatrix = new int[50, 50];
+        positionMatrix = new GridObject[50, 50];
         for (int i = 0; i < positionMatrix.GetLength(0); i++)
         {
             for (int j = 0; j < positionMatrix.GetLength(1); j++)
             {
+                positionMatrix[i, j] = new GridObject();
                 if (!Physics.Raycast(new Vector3((float)(0.5 + 1*i), 1, (float)(0.5 + 1*j)), Vector3.down, 20f))
                 {
-                    positionMatrix[i, j] = 2;
+                    positionMatrix[i, j] = new GridObject(2);
                     print(string.Format("I: {0}, J: {1}, Bool: {2}", i, j, false));
                 } else
                 {
@@ -23,13 +44,13 @@ public class Map : MonoBehaviour
                 }
             }
         }
-        positionMatrix[1, 0] = 1;
+        positionMatrix[1, 0].Type = 1;
         spawnEnemy(1, 3);
     }
 
     void spawnEnemy(int x, int y)
     {
-        positionMatrix[x, y] = 3;
+        positionMatrix[x, y].Type = 3;
         GameObject skeleton = Instantiate(skeletonPrefab, transform.position, transform.rotation) as GameObject;
         skeleton.transform.position = new Vector3(x+.5f,0,y+.5f);
         skeleton.transform.localScale *= .5f;
@@ -38,11 +59,6 @@ public class Map : MonoBehaviour
     }
     void despawnEnemy(int x, int y)
     {
-        positionMatrix[x, y] = 0;
-    }
-
-    void Update()
-    {
-
+        positionMatrix[x, y].Type = 0;
     }
 }
